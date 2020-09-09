70680fa8befe15d39e395c5134a519d8645b0816
Liangyu <liangyuhuang@outlook.com>
2020-09-09 14:53:15 +0800

    init project

---
+++ src/Customer.java
@@ -0,0 +1,56 @@
+package edu.unsw.project1;
+
+public class Customer {
+    /*
+        String name ：客户姓名
+        char gender  ：性别
+        int age          ：年龄
+        String phone：电话号码
+        String email ：电子邮箱
+     */
+    private String name;
+    private char gender;
+    private int age;
+    private String phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/CustomerList.java
@@ -0,0 +1,28 @@
+package edu.unsw.project1;
+
+public class CustomerList {
+
+    public CustomerList(int totalCustomer){
+
+    }
+
+    public boolean addCustomer(Customer customer){
+
+    }
+
+    public boolean replaceCustomer(int index, Customer cust){
+
+    }
+
+    public boolean deleteCustomer(int index){
+
+    }
+
+    public Customer[] getAllCustomers(){
+
+    }
+
+    public Customer getCustomer(int index){
+
+    }
+}
